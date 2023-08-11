4b845dca1048a83e2693873c89b10a8dac4daa99
fucklinux <1205499078@qq.com>
2023-08-11 18:46:25 +0800

    1.3.0 版本发布

---
+++ update.js
@@ -0,0 +1,16 @@
+//设置当前版本数据
+var versionName = "1.3.0";
+var versionCode = 103000;
+var buildDate = "2023年8月11日";
+//函数：获取最新版本数据
+function checkUpdate() {
+    var getUpdateData = new XMLHttpRequest();
+    getUpdateData.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            updateData = JSON.parse(this.responseText);
+            processUpdate();
+        }
+    };
+    getUpdateData.open("GET", "https://bgdroid.netlify.app/version.json", true);
+    getUpdateData.send();
+}
