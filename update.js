9e093d47f8d34ba4298cc37db21d8922507b62d0
Fuck Linux <89513519+fucklinux@users.noreply.github.com>
2023-10-02 13:21:00 +0800

    v1.10.0 版本发布
    -新增 倒数日

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 //设置当前版本数据
-var versionName = "1.9.0";
-var versionCode = 109000;
-var buildDate = "2023年9月24日";
+var versionName = "1.10.0";
+var versionCode = 231002;
+var buildDate = "2023年10月2日";
 //函数：获取最新版本数据
 function checkUpdate() {
     var getUpdateData = new XMLHttpRequest();
